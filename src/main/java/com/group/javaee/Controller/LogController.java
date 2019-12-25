9b597665423caefd26c739a8ce3455c414d2cf26
Lin002 <463657869@qq.com>
2019-12-25 14:38:59 +0800

    J2EE课设

---
+++ src/main/java/com/group/javaee/Controller/LogController.java
@@ -0,0 +1,14 @@
+package com.group.javaee.Controller;
+
+import com.group.javaee.Pojo.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class LogController {
+
+}
